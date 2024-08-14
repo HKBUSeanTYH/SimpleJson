d2985e1634204c1b296e86a88644b2920be011df
HKBUSeanTYH <seantyh118@gmail.com>
2024-08-14 22:52:05 +0800

    faulty base struct/union

--- include/JsonValue.h
+++ include/JsonValue.h
@@ -1,4 +1,21 @@
 #ifndef JSON_VALUE_DEF
 #define JSON_VALUE_DEF
 
+#include <memory>
+#include <map>
+
+struct NullHolder {
+
+};
+
+union JsonValue {
+    int i;
+    double d;
+    bool b;
+    NullHolder null;
+    std::string s;
+    std::unique_ptr<std::vector<JsonValue>> jsonArr;
+    std::unique_ptr<std::map<std::string, JsonValue>> jsonObj; //allows pointing to nested json obj/array
+};
+
 #endif
