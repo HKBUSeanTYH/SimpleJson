279568760d3fe28501c87af23e7807eb7ea85030
HKBUSeanTYH <seantyh118@gmail.com>
2024-08-18 22:15:07 +0800

    1. setup struct overloaded trick in case i want to implement inserters

--- include/JsonValue.h
+++ include/JsonValue.h
@@ -12,7 +12,11 @@
 
 struct JsonValue;
 struct JsonValue {
-    std::variant<std::monostate, std::nullptr_t, int, double, bool, std::string, std::vector<JsonValue>, std::map<std::string, JsonValue>> data_;
+    template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+    template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+    using JsonVariant = std::variant<std::monostate, std::nullptr_t, int, double, bool, std::string, std::vector<JsonValue>, std::map<std::string, JsonValue>>;
+    JsonVariant data_;
 };
 std::istream& operator >>(std::istream&, JsonValue&);
 // std::ostream& operator <<(std::ostream&, const JsonValue&);
